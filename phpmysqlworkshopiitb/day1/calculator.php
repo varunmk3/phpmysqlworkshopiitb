f2033ffd5d591d5d3e3ca9383c68fb9206d0ca58
varunmk3 <72083708+varunmk3@users.noreply.github.com>
2020-10-03 00:07:52 +0530

    first commit

---
+++ phpmysqlworkshopiitb/day1/calculator.php
@@ -0,0 +1,20 @@
+<?php
+        
+         $a = 30;
+         $b = 25;
+         
+         $c = $a + $b;
+         echo "Addtion Operation Result: $c <br/>";
+         
+         $c = $a - $b;
+         echo "Substraction Operation Result: $c <br/>";
+         
+         $c = $a * $b;
+         echo "Multiplication Operation Result: $c <br/>";
+         
+         $c = $a / $b;
+         echo "Division Operation Result: $c <br/>";
+         
+    
+      
+?>
