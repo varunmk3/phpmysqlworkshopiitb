7ef72fe379a01ce4368a46132a85a0c2b8c6a54b
varunmk3 <72083708+varunmk3@users.noreply.github.com>
2020-10-07 00:48:10 +0530

    day4

--- phpmysqlworkshopiitb/day3/connect.php
+++ phpmysqlworkshopiitb/day3/connect.php
@@ -1,23 +1,15 @@
-
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "varun";
+$dbname = "result";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if (mysqli_query($conn, $sql)) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>
+echo "<h2>Connected successfully<h2><br>";
+?> 
